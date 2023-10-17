ae81240a9ee676e4db036cfab2956cf57faa8df7
andrewbelfiglio <belfiga@pmacs.upenn.edu>
2023-10-17 01:47:46 -0400

    added andrew's initial mapping files

---
+++ andrew.js
@@ -0,0 +1,76 @@
+// Creating the map object
+let myMap = L.map("map", {
+    center: [41.203323, -77.194527],
+    zoom: 7
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
+  // Load the GeoJSON data.
+  let geoData = "Resources/andrew.geojson";
+  
+  // Get the data with d3.
+  d3.json(geoData).then(function(data) {
+  
+    console.log(data);
+
+    // Create a new choropleth layer.
+  let geojson = L.choropleth(data, {
+
+    // Define which property in the features to use.
+    valueProperty: "pupil_expend",
+    // USING avts_sendi AS A PLACEHOLDER UNTIL I ADD DATA TO GEOJSON FILE
+
+    // Set the color scale.
+    scale: ["#f7fcf5", "#00441b"],
+
+    // The number of breaks in the step range
+    steps: 10,
+
+    // q for quartile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+
+    // Binding a popup to each layer
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<strong>" + feature.properties.school_dis + "</strong><br /><br />Per Pupil Expenditure 2021-22: " +
+        feature.properties.pupil_expend);
+    }
+  }).addTo(myMap);
+
+  // Set up the legend.
+  let legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    let limits = geojson.options.limits;
+    let colors = geojson.options.colors;
+    let labels = [];
+
+    // Add the minimum and maximum.
+    let legendInfo = "<h1>Per Pupil Expenditure</h1>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding the legend to the map
+  legend.addTo(myMap);
+  })
