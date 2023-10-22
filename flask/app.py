08232a5ef99a51e09d163ee5a5128719a8fdb50a
Jacob Field <jacob.field@gmail.com>
2023-10-21 23:46:41 -0400

    map is working, needs some CSS tweeks

--- flask/app.py
+++ flask/app.py
@@ -47,66 +47,190 @@ def home():
     return render_template("home.html", query_results=results)
 
 
-# Run a query from a POST
-@app.route("/post/<post_id>")
-def get_post(post_id):
-
-    def parse_post(post_id):
-        # do something to get query out of post
-        return post_id
-
-    query = parse_post(post_id)
-
-    def run_query(query):
-        # run a query
-        return query
-
-    return render_template("home.html", query_result=run_query(query))
-
-
-# Provide an API
-@app.route("/api/test")
-def api_test():
-
-    results = ['Populate this with new dics for all the data needed to pass',
-               ['nested', 'lists', 'work'],
-               {'1. dictionaries': 'Work here too!',
-                '2. as do': {'nested': 'dicts'}
-                },
-               'So, tell me what data you need and I\'ll serve it through a variable',
-               'specifically "query_results',
-               'and it will pull in for each page'
-               ]
-
-    return jsonify(results)
-
-
 # calls the geojson file and passes it to a js var
 @app.route("/map")
 def get_map():
-    results = 'RESULTS'
-    boundaries = ''
+    js_file = url_for('static', filename='map/andrew2.js')
+    # css_file = url_for('static', filename='map/andrew.css')
+    css_file = ""
+    data = ''
 
-    with open('static/Pennsylvania School Districts Boundaries.geojson') as file:
-        boundaries = json.load(file)
+    with open('static/map/andrew.geojson') as file:
+        data = json.load(file)
 
-    return render_template("map.html", query_results=results, boundaries=boundaries)
+    note = """<p>Use this map to explore how your school district compares to its neighboring districts and to the state overall.
+        <p>Notice the clustering of Per Pupil Expenditure...
+        <p>We see generally see the highest per pupil expenditures in Pittsburgh and its surrounding suburbs, in the Philadelphia suburbs but not the city itself, in Indiana and the surrounding districts, and in the fringes of the northeast. We generally see the lowest per pupil expenditures throughout south-central PA and the heart of the northeast.
+        <p>However, we can see that academic Proficiency does not map directly onto these expenditure patterns by noticing how the geographical pattern of lowest per pupil expenditures \"disappears\" when viewing rates of Proficiency. One may expect that more money spent would result in better academic outcomes, but we are seeing that may not always be the case.
+        <p>For that reason, we want to explore how related expenditure is to academic proficiency in Pennsylvania. Click onto the Scatter plot to explore this question."""
+
+    attribution = '''<p>This is 2018-2019 data from the 
+    <a class="underline" href="https://www.education.pa.gov/DataAndReporting/Pages/default.aspx">Pennsylvania Department of Education</a>.
+    The Proficiency data is from the Keystone Exams, which replaced what many will know as the "PSSAs",
+    and are administered to 11th graders (with some exceptions for those with special needs).'''
+
+    controls = '''
+        <!-- Code for tailwind radio buttons https://www.material-tailwind.com/docs/html/radio-button -->
+        <div class="flex gap-10 radio-buttons-container">
+          <div <h1>Select a Metric to View</h1></div>
+          <div class="inline-flex items-center">
+            <label
+              class="relative flex cursor-pointer items-center rounded-full p-3 text-green-900 border-green-900"
+              for="pupil_expend"
+              data-ripple-dark="true"
+            >
+              <input
+                id="pupil_expend"
+                name="type"
+                type="radio"
+                class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-green-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-green-900 hover:before:bg-green-900 hover:before:opacity-10"
+                checked
+                />
+              <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-green-900 opacity-0 transition-opacity peer-checked:opacity-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-3.5 w-3.5"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                >
+                  <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                </svg>
+              </div>
+            </label>
+            <label
+              class="mt-px cursor-pointer select-none font-light text-gray-700"
+              for="pupil_expend"
+            >
+              Per Pupil Expenditure
+            </label>
+          </div>
+          <div class="inline-flex items-center">
+            <label
+              class="relative flex cursor-pointer items-center rounded-full p-3 text-indigo-700 border-indigo-700"
+              for="algebra"
+              data-ripple-dark="true"
+            >
+              <input
+                id="algebra"
+                name="type"
+                type="radio"
+                class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-indigo-700 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-indigo-700 hover:before:bg-indigo-700 hover:before:opacity-10"
+              />
+              <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-indigo-700 opacity-0 transition-opacity peer-checked:opacity-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-3.5 w-3.5"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                >
+                  <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                </svg>
+              </div>
+            </label>
+            <label
+              class="mt-px cursor-pointer select-none font-light text-gray-700"
+              for="algebra"
+            >
+              Algebra Proficiency
+            </label>
+          </div>
+          <div class="inline-flex items-center">
+            <label
+              class="relative flex cursor-pointer items-center rounded-full p-3 text-fuchsia-800 border-fuchsia-800"
+              for="literature"
+              data-ripple-dark="true"
+            >
+              <input
+                id="literature"
+                name="type"
+                type="radio"
+                class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-fuchsia-800 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-fuchsia-800 hover:before:bg-fuchsia-800 hover:before:opacity-10"
+              />
+              <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-fuchsia-800 opacity-0 transition-opacity peer-checked:opacity-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-3.5 w-3.5"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                >
+                  <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                </svg>
+              </div>
+            </label>
+            <label
+              class="mt-px cursor-pointer select-none font-light text-gray-700"
+              for="literature"
+            >
+              Literature Proficiency
+            </label>
+          </div>
+          <div class="inline-flex items-center">
+            <label
+              class="relative flex cursor-pointer items-center rounded-full p-3 text-green-800 border-green-800"
+              for="biology"
+              data-ripple-dark="true"
+            >
+              <input
+                id="biology"
+                name="type"
+                type="radio"
+                class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-green-800 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-green-800 hover:before:bg-green-800 hover:before:opacity-10"
+              />
+              <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-green-800 opacity-0 transition-opacity peer-checked:opacity-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-3.5 w-3.5"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                >
+                  <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                </svg>
+              </div>
+            </label>
+            <label
+              class="mt-px cursor-pointer select-none font-light text-gray-700"
+              for="biology"
+            >
+              Biology Proficiency
+            </label>
+          </div>
+        </div>
+    '''
+
+    return render_template("vis.html", 
+                           js=js_file,
+                           css=css_file,
+                           controls=controls,
+                           data=data,
+                           note=note,
+                           attribution=attribution)
 
 
-@app.route("/scatter")
-def get_scatter():
-    return render_template("scatter.html")
+# @app.route("/scatter")
+# def get_scatter():
+#     js_file = url_for('static', filename='app.js')
+#     data="",
+#     note="Notes here"
+#     attribution="Attribution here"
+#     return render_template("vis.html", js=js_file, data=data, note=note, attribution=attribution)
 
 
-@app.route("/slope")
-def get_slope():
-    return render_template("slope.html")
+# @app.route("/slope")
+# def get_slope():
+#     js_file = url_for('static', filename='app.js')
+#     data="",
+#     note="Notes here"
+#     attribution="Attribution here"
+#     return render_template("vis.html", js=js_file, data=data, note=note, attribution=attribution)
 
 
-@app.route("/radar")
-def get_radar():
-    return render_template("radar.html")
-
+# @app.route("/radar")
+# def get_radar():
+#     js_file = url_for('static', filename='app.js')
+#     data="",
+#     note="Notes here"
+#     attribution="Attribution here"
+#     return render_template("vis.html", js=js_file, data=data, note=note, attribution=attribution)
 
 # set debugger
 if __name__ == "main":
---
+++ flask/static/map/andrew2.js
@@ -0,0 +1,112 @@
+// Creating the map object
+let myMap = L.map("chart", {
+    center: [41, -77.5],
+    zoom: 7.5
+  });
+  
+  // Adding the tile layer
+  // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  // }).addTo(myMap);
+  
+  // Load the GeoJSON data.
+  let geoData = data
+  
+  // Get the data with d3.
+    let valueProperty = "pupil_expend"; // Default valueProperty
+    let geojson; // Define geojson globally
+    let scale;
+    let legend;
+  
+    // Function to update the choropleth layer with the selected valueProperty
+    function updateMap(valueProp) {
+      if (geojson) {
+        myMap.removeLayer(geojson);
+      }
+      if (legend) {
+        myMap.removeControl(legend);
+      }
+
+      if (valueProp === "pupil_expend") {
+        scale = ["#f7fcf5", "#00441b"]; // Scale for pupil_expend
+      } else if (valueProp === "algebra") {
+        scale = ["#ffffcc", "#a1dab4", "#41b6c4", "#2c7fb8", "#253494"]; // Scale for algebra
+      } else if (valueProp === "literature") {
+        scale = ["#edf8fb", "#b3cde3", "#8c96c6", "#8856a7", "#810f7c"]; // Scale for literature
+      } else if (valueProp === "biology") {
+        scale = ["#ffffcc", "#c2e699", "#78c679", "#31a354", "#006837"]; // Scale for biology
+      }
+  
+      geojson = L.choropleth(data, {
+        valueProperty: valueProp,
+        scale: scale,
+        steps: 20,
+        mode: "q",
+        style: {
+          color: "#fff",
+          weight: 1,
+          fillOpacity: 0.8
+        },
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup('<h2>' + feature.properties.school_dis + '</h2> <strong>Per Pupil Expenditure 2018-19:</strong> $' +
+          feature.properties.pupil_expend + '<br />' + '<strong>Algebra Proficiency: </strong>' + feature.properties.algebra +
+          '%' + '<br />' + '<strong>Literature Proficiency: </strong>' + feature.properties.literature + '%' + '<br />' +
+          '<strong>Biology Proficiency: </strong>' + feature.properties.biology + '%');
+        }
+      }).addTo(myMap);
+
+      updateLegend(geojson, valueProp, scale);
+    }
+  
+      // Set up the legend.
+    function updateLegend(geojson, valueProp, scale) {
+        legend = L.control({ position: "bottomright" });
+        legend.onAdd = function() {
+          let div = L.DomUtil.create("div", "info legend");
+          let limits = geojson.options.limits;
+          let colors = geojson.options.colors;
+          let labels = [];
+      
+          let legendTitle;
+          if (valueProp === "pupil_expend") {
+            legendTitle = "Per Pupil Expenditure"; // Legend title for pupil_expend
+          } else if (valueProp === "algebra") {
+            legendTitle = "Percent Proficient in Algebra"; // Legend title for algebra
+          } else if (valueProp === "literature") {
+            legendTitle = "Percent Proficient in Literature"; // Legend title for literature
+          } else if (valueProp === "biology") {
+            legendTitle = "Percent Proficient in Biology"; // Legend title for biology
+          }
+  
+          // Add the legend heading and minimum and maximum.
+          let legendInfo = "<h1>" + legendTitle + "</h1>" +
+          "<div class=\"labels\">" +
+          "<div class=\"min\">" + limits[0] + "</div>" +
+          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+          "</div>";
+  
+          div.innerHTML = legendInfo;
+  
+          limits.forEach(function(limit, index) {
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+          });
+  
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+      };
+  
+      // Adding the legend to the map
+      legend.addTo(myMap);
+    }
+  
+    // Dropdown change event listener
+    let radioButtons = document.querySelectorAll('input[type="radio"]');
+    radioButtons.forEach((radio) => {
+      radio.addEventListener("change", function () {
+        valueProperty = this.id;
+        updateMap(valueProperty);
+      });
+    });
+  
+    // Initial map generation
+    updateMap(valueProperty);
